8dde618693504879fe4c9378d49dee7ad47a0f9a
mopanowicz <marek.opanowicz@gmail.com>
2025-07-08 21:13:38 +0200

    Use the library

--- examples/spring-multimodule/application/src/main/java/com/example/application/Application.java
+++ examples/spring-multimodule/application/src/main/java/com/example/application/Application.java
@@ -1,10 +1,25 @@
 package com.example.application;
 
+import com.example.library.component.SimpleComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
+@RestController
 public class Application {
+
+	private final SimpleComponent simpleComponent;
+
+	public Application(SimpleComponent simpleComponent) {
+		this.simpleComponent = simpleComponent;
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return simpleComponent.message();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
