c8910ff46c40c3d4546788a59bb1bdd2a2767daa
mopanowicz <marek.opanowicz@gmail.com>
2025-07-22 11:47:02 +0200

    Add timestamp and count to the message

--- examples/spring-multimodule/application/src/main/java/com/example/application/Application.java
+++ examples/spring-multimodule/application/src/main/java/com/example/application/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication(scanBasePackages = "com.example")
@@ -31,7 +32,7 @@ public class Application {
 	@GetMapping("/")
 	public String home() {
 		String message = simpleComponent.message();
-		kafkaEventLogger.logEvent(message);
+		kafkaEventLogger.logEvent(LocalDateTime.now() +" "+ count.incrementAndGet() +" "+ message);
 //		log.info("home message={}", message);
 		return message;
 	}
