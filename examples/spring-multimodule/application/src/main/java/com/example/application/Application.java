6a28b9eebf379fa0cf2da842666c9ffe8acb9a35
mopanowicz <marek.opanowicz@gmail.com>
2025-07-23 11:15:53 +0200

    Refactor application and remove library

--- examples/spring-multimodule/application/src/main/java/com/example/application/Application.java
+++ examples/spring-multimodule/application/src/main/java/com/example/application/Application.java
@@ -1,41 +1,10 @@
 package com.example.application;
 
-import com.example.library.component.SimpleComponent;
-import com.example.logging.KafkaEventLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication(scanBasePackages = "com.example")
-@RestController
 public class Application {
-
-	private final SimpleComponent simpleComponent;
-	private final KafkaEventLogger kafkaEventLogger;
-	private final AtomicInteger count = new AtomicInteger();
-
-	private final String batchId = String.valueOf(UUID.randomUUID());
-
-	public Application(SimpleComponent simpleComponent, KafkaEventLogger kafkaEventLogger) {
-		this.simpleComponent = simpleComponent;
-		this.kafkaEventLogger = kafkaEventLogger;
-	}
-
-	@GetMapping("/")
-	Message home() {
-		Message message = new Message();
-		message.setTimestamp(LocalDateTime.now());
-		message.setBatchId(batchId);
-		message.setCount(count.getAndIncrement());
-		message.setCargo(simpleComponent.message());
-		kafkaEventLogger.logEvent(message);
-		return message;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ examples/spring-multimodule/application/src/main/java/com/example/application/Event.java
@@ -0,0 +1,19 @@
+package com.example.application;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class Event {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDateTime timestamp;
+    private String batchId;
+    private Integer count;
+    private String cargo;
+
+}
